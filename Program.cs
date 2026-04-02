ea4309b8c846b00dcb4c80fe7ce35748d891ab2e
Vishnu Karthick.G <gvishnukarthick@Vishnus-MacBook-Air.local>
2026-04-02 16:36:57 +0530

    port change

--- Program.cs
+++ Program.cs
@@ -40,7 +40,7 @@ builder.Services.AddCors(options =>
     options.AddPolicy("AllowFrontend", policy =>
     {
         policy
-            .WithOrigins("http://localhost:5173")
+            .WithOrigins("https://zyphor-client.vercel.app/")
             .AllowAnyHeader()
             .AllowAnyMethod()
             .AllowCredentials();
